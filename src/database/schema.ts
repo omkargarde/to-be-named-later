92a87a1f9e4d4e5a1c3d7aa847377c12a039e70e
omkar garde <omyagarde@gmail.com>
2026-02-01 18:18:38 +0530

    create orders table

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -5,7 +5,7 @@ export const usersTable = sqliteTable("users_table", {
   id: integer("id").notNull().primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
-  password: text("password").notNull(),
+  passwordHash: text("password_hash").notNull(),
   role: text("role", { enum: ["user", "admin"] }).notNull(),
   createdAt: text("created_at")
     .notNull()
@@ -20,9 +20,38 @@ export const productsTable = sqliteTable("products_table", {
   id: integer("id").notNull().primaryKey(),
   name: text("name").notNull(),
   description: text("description"),
-  price: numeric("price", { mode: "number" }),
+  price: numeric("price").notNull(),
   category: text("category"),
-  isActive: integer("is_active", { mode: "boolean" }),
+  isActive: integer("is_active", { mode: "boolean" }).default(sql`true`),
+  createAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(current_timestamp)`)
+    .$onUpdateFn(() => sql`(current_timestamp)`),
+});
+
+const ordersTable = sqliteTable("orders_table", {
+  id: integer("id").notNull().primaryKey(),
+  userId: integer("user_id")
+    .references(() => usersTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  status: text("status", {
+    enum: [
+      "pending",
+      "active",
+      "draft",
+      "out_of_stock",
+      "archived",
+      "rejected",
+    ],
+  })
+    .notNull()
+    .default("pending"),
+  totalAmount: numeric("total_amount").notNull(),
   createAt: text("created_at")
     .notNull()
     .default(sql`(current_timestamp)`),
