6c081af2dabaa2ce0a39cbbf95b543287648c2a2
omkar garde <omyagarde@gmail.com>
2026-02-01 14:30:49 +0530

    created products table schema

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,13 +1,29 @@
 import { sql } from "drizzle-orm";
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, numeric, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users_table", {
-  id: int().notNull().primaryKey(),
-  name: text().notNull(),
-  email: text().notNull().unique(),
-  password: text().notNull(),
+  id: integer("id").notNull().primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
   role: text("role", { enum: ["user", "admin"] }).notNull(),
   createdAt: text("created_at")
+    .notNull()
+    .default(sql`(current_timestamp)`),
+  updatedAt: text("updated_at")
+    .notNull()
+    .default(sql`(current_timestamp)`)
+    .$onUpdateFn(() => sql`(current_timestamp)`),
+});
+
+export const productsTable = sqliteTable("products_table", {
+  id: integer("id").notNull().primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+  price: numeric("price", { mode: "number" }),
+  category: text("category"),
+  isActive: integer("is_active", { mode: "boolean" }),
+  createAt: text("created_at")
     .notNull()
     .default(sql`(current_timestamp)`),
   updatedAt: text("updated_at")
