703a738712dae7a57396f83caaf86cea4850d3ce
omkar garde <omyagarde@gmail.com>
2026-03-01 13:31:50 +0530

    reorganise reponse types

--- src/types/response.ts
+++ src/types/response.ts
@@ -3,25 +3,24 @@ import { HTTP_STATUS_CODE_VALUES } from "@/utils/http-status-codes";
 export interface IApiError {
   code: HTTP_STATUS_CODE_VALUES;
   message: string;
-  details: string[];
+  details: Array<string>;
 }
 export type TApiErrorResponse = { ok: false; error: IApiError };
 
-// pagination.ts
+export interface IApiSuccess<T> {
+  code: HTTP_STATUS_CODE_VALUES;
+  values: T;
+}
+export type TApiSuccessResponse<T> = { ok: true; data: IApiSuccess<T> };
+
 export interface Pagination {
   page: number;
   pageSize: number;
   totalItems: number;
   totalPages: number;
 }
-
-export interface IApiSuccess<T> {
-  code: HTTP_STATUS_CODE_VALUES;
-  values: T;
+export interface TApiResponseWithPagination<T> extends TApiSuccessResponse<T> {
+  pagination: Pagination;
 }
 
-export type TApiSuccessResponse<T> = { ok: true; data: IApiSuccess<T> };
-
 export type TFetchResponse<T> = TApiSuccessResponse<T> | TApiErrorResponse;
-
-export type TApiResponseWithPagination<T> = { ok: true; data: T; pagination: Pagination };
